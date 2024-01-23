6eb7186dd441a28cddebf86805637216bcef0cc1
Mahendi Hushen <mahendi@praella.com>
2024-01-23 14:54:35 +0530

    VR section updated

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,66 @@
 gsap.registerPlugin(ScrollTrigger)
+let mm = gsap.matchMedia();
+mm.add("(max-width: 767px)", () => {
+ 
+    let mytl2 = gsap.timeline()
 
-let mytl = gsap.timeline()
-.to('.banner__content', {opacity: 0})
-ScrollTrigger.create({
-    trigger: ".vr-wrapper",
-    animation: mytl,
-    start: "top 5%",
-    end: "top 5%",
-    scrub: 1,
-    markers: false,
+    .to(".vrimg-wrapper img", {
+       keyframes: {
+       "0%": { scale: 1, opacity: 1 },
+       "20%": { scale: 1.8, opacity: 1 },
+       "50%": { scale: 2.2, opacity: 1 },
+       "90%": { scale: 2.4, opacity: 0 }
+       },
+       transformOrigin: "center center"
+   })
+   ScrollTrigger.create({
+       trigger: ".vr-wrapper",
+       animation: mytl2,
+       start: "50% 50%",
+       end: "100% 50%",
+       scrub: 1,
+       markers: false,
+       pin: true,
+       transformOrigin: "center center",
+   });
 });
 
-let mytl2 = gsap.timeline()
- .to(".vrimg-wrapper img", {scale:2, duration: 2})
- .to(".vrimg-wrapper img", {opacity:0, duration: 1})
 
-ScrollTrigger.create({
-    trigger: ".vr-wrapper",
-    animation: mytl2,
-    scrub: 2,
-    markers: false,
-    start: 'top top',
-    end: '100% bottom',
-    pin: true
+mm.add("(min-width: 768px)", () => {
+    // let mytl = gsap.timeline()
+    // .to('.banner__content', {opacity: 0})
+    // ScrollTrigger.create({
+    //     trigger: ".vr-wrapper",
+    //     animation: mytl,
+    //     start: "top 5%",
+    //     end: "top 5%",
+    //     scrub: 1,
+    //     markers: false,
+    // });
+
+    let mytl2 = gsap.timeline()
+
+    .to(".vrimg-wrapper img", {
+        keyframes: {
+        "0%": { scale: 1, opacity: 1 },
+        "20%": { scale: 1.5, opacity: 1 },
+        "40%": { scale: 2, opacity: 1 },
+        "60%": { scale: 2.5, opacity: 1 },
+        "80%": { scale: 2.8, opacity: 0 },
+        "100%": { scale: 3, opacity: 0}
+        },
+        transformOrigin: "center center",
+        ease: "power1.out"
+    })
+    ScrollTrigger.create({
+        trigger: ".vr-wrapper",
+        animation: mytl2,
+        start: "50% 50%",
+        end: "150% 50%",
+        scrub: 0.5,
+        markers: false,
+        pin: true,
+        transformOrigin: "center center",
+    });
+
 });
